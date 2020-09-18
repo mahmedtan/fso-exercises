f09daae1f2bad47b0afaabc27c43c7e50e2dc308
Muhammad Ahmed <mahmedexec@gmail.com>
2020-09-18 21:45:56 +0500

    1.5: course information step5

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -44,18 +44,20 @@ const Total = (props) => {
 };
 
 const App = () => {
-  const course = "Half stack application development";
-  const parts = [
-    { name: "fundamentals of React", exercises: 10 },
-    { name: "Using props to pass data", exercises: 7 },
-    { name: "State of a component", exercises: 14 },
-  ];
+  const course = {
+    name: "Half stack application development",
+    parts: [
+      { name: "fundamentals of React", exercises: 10 },
+      { name: "Using props to pass data", exercises: 7 },
+      { name: "State of a component", exercises: 14 },
+    ],
+  };
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 };
