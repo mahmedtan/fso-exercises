e661d2a062d86b6f6ee354d4367f394ca35d6054
Muhammad Ahmed <mahmedexec@gmail.com>
2020-09-19 16:16:30 +0500

    1.8: unicafe step3

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -13,19 +13,9 @@ const Content = ({ text, value }) => (
   </p>
 );
 
-const App = () => {
-  const [good, setGood] = useState(0);
-  const [bad, setBad] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-
+const Statistics = ({ good, bad, neutral }) => {
   return (
     <div>
-      <Header text="give feedback" />
-
-      <Button handleClick={() => setGood(good + 1)} text="good" />
-      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
-      <Button handleClick={() => setBad(bad + 1)} text="bad" />
-
       <Header text="statistics" />
       <Content text={"good"} value={good} />
       <Content text={"neutral"} value={neutral} />
@@ -39,6 +29,24 @@ const App = () => {
         text={"positive"}
         value={`${(good / (good + neutral + bad)) * 100} %`}
       />
+    </div>
+  );
+};
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+
+  return (
+    <div>
+      <Header text="give feedback" />
+
+      <Button handleClick={() => setGood(good + 1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+
+      <Statistics good={good} bad={bad} neutral={neutral} />
     </div>
   );
 };
