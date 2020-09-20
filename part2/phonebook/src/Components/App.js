4771b4bae58d821a306cfd757f755d060d99279b
Muhammad Ahmed <mahmedexec@gmail.com>
2020-09-20 21:56:04 +0500

    2.11: The Phonebook Step6

--- part2/phonebook/src/Components/App.js
+++ part2/phonebook/src/Components/App.js
@@ -1,18 +1,20 @@
-import React, { useState } from "react";
+import Axios from "axios";
+import React, { useState, useEffect } from "react";
 import Filter from "./Filter";
 import PersonForm from "./PersonForm";
 import Persons from "./Persons";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    Axios.get("http://localhost:3001/persons").then((result) => {
+      setPersons(result.data);
+    });
+  }, [setPersons]);
 
   const handleNameChange = (e) => {
     setNewName(e.target.value);
@@ -58,10 +60,7 @@ const App = () => {
       <Persons filter={filter} persons={persons} />
 
       {/* <pre style={{ backgroundColor: "MistyRose", margin: 30, padding: 10 }}>
-        <code>
-          Debug:{" "}
-          {JSON.stringify({ newName, newNumber, filter, persons }, null, 4)}
-        </code>
+        <code>Debug: {JSON.stringify({ persons }, null, 4)}</code>
       </pre> */}
     </div>
   );
