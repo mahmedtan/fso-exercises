43f8516c05fae4878a685fc1a6a91392af24371a
Muhammad Ahmed <mahmedexec@gmail.com>
2020-09-20 17:57:18 +0500

    2.9*: The Phonebook Step4

--- part2/phonebook/src/Components/App.js
+++ part2/phonebook/src/Components/App.js
@@ -1,9 +1,15 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" },
+  ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [filter, setFilter] = useState("");
 
   const handleNameChange = (e) => {
     setNewName(e.target.value);
@@ -23,15 +29,27 @@ const App = () => {
     setNewName("");
   };
 
-  const names = persons.map((person) => (
-    <p key={person.name}>
-      {person.name} {person.number}
-    </p>
-  ));
+  const handleFilterChange = (e) => {
+    setFilter(e.target.value);
+  };
+
+  const reg = new RegExp(filter, "i");
+  const names = persons
+    .filter((person) => reg.test(person.name))
+    .map((person) => (
+      <p key={person.name}>
+        {person.name} {person.number}
+      </p>
+    ));
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with{" "}
+        <input type="text" value={filter} onChange={handleFilterChange} />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addNewName}>
         <div>
           name:{" "}
@@ -49,6 +67,13 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       {names}
+
+      {/* <pre style={{ backgroundColor: "MistyRose", margin: 30, padding: 10 }}>
+        <code>
+          Debug:{" "}
+          {JSON.stringify({ newName, newNumber, filter, persons }, null, 4)}
+        </code>
+      </pre> */}
     </div>
   );
 };
