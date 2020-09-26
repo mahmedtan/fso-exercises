b3b1f85d9f5f46ecb60e3d5e0f1dff2688d7e4a6
Muhammad Ahmed <mahmedexec@gmail.com>
2020-09-26 23:46:31 +0500

    3.11 phonebook full stack

---
+++ part2/phonebook/build/precache-manifest.608e32e70ede9bbc2f83e26a034b8ec3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "166aa8ba2f29f9f8e78a4d85e56b4c91",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19415dc4d7ff24de94f6",
+    "url": "/static/css/main.b657f708.chunk.css"
+  },
+  {
+    "revision": "5025582f0bba5a0d1c75",
+    "url": "/static/js/2.d0ffda05.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d0ffda05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19415dc4d7ff24de94f6",
+    "url": "/static/js/main.ff4d18c2.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
