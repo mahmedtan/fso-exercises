5fe2b6f6a941c54258e8e9e35d74318b3835717e
Muhammad Ahmed <mahmedexec@gmail.com>
2020-09-28 23:24:27 +0500

    4.5*: helper functions and unit tests, step3

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,7 +1,13 @@
 const dummy = (blogs) => 1;
 
 const totalLikes = (blogs) => blogs.reduce((sum, item) => sum + item.likes, 0);
+
+const favoriteBlog = (blogs) => {
+  const arrNum = blogs.map((item) => item.likes);
+  return blogs.find((item) => Math.max(...arrNum) === item.likes);
+};
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
