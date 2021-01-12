a1ce93ab51b13836b92873118ddc75b2d1802273
Ahmed Tan <mahmedexec@gmail.com>
2021-01-12 17:31:49 +0500

    8.8: Authors view

---
+++ part8/library-frontend/src/components/Authors.js
@@ -0,0 +1,42 @@
+import { useQuery } from "@apollo/client";
+import React, { useEffect, useState } from "react";
+import { ALL_AUTHORS } from "../query";
+
+const Authors = (props) => {
+  const result = useQuery(ALL_AUTHORS);
+  const [authors, setAuthors] = useState([]);
+
+  useEffect(() => {
+    if (result.data) {
+      setAuthors(result.data.allAuthors);
+    }
+  }, [result]);
+
+  if (!props.show) {
+    return null;
+  }
+
+  return (
+    <div>
+      <h2>authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>born</th>
+            <th>books</th>
+          </tr>
+          {authors.map((a) => (
+            <tr key={a.name}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Authors;
---
+++ part8/library-frontend/src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  uri: "http://localhost:4000",
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
