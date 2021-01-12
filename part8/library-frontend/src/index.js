6166efe552e8fc0a0e7cca7b1146a4f48a7e3b94
Ahmed Tan <mahmedexec@gmail.com>
2021-01-12 17:56:15 +0500

    8.9: Books view

--- part8/library-frontend/src/index.js
+++ part8/library-frontend/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./global.css";
 import App from "./App";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 const client = new ApolloClient({
--- part8/library-frontend/src/query.js
+++ part8/library-frontend/src/query.js
@@ -9,3 +9,12 @@ export const ALL_AUTHORS = gql`
     }
   }
 `;
+export const ALL_BOOKS = gql`
+  query {
+    allBooks {
+      title
+      author
+      published
+    }
+  }
+`;
