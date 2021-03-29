e5beabea2b7da17714913ab60be9dc8884d4d3af
Ahmed Tan <mahmedexec@gmail.com>
2021-03-29 16:52:17 +0500

    8.4: Books of an author

--- part8/libraryServer/index.js
+++ part8/libraryServer/index.js
@@ -96,7 +96,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `;
@@ -105,7 +105,8 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) =>
+      args.author ? books.filter((book) => book.author === args.author) : books,
     allAuthors: () =>
       authors.map((author) => ({
         ...author,
